bbb3b649492ffc4b922948e194a292066aca2e30
Taylor Bell <taylorbell@gmail.com>
2017-12-14 17:19:26 -0700

    check if user is enrolled

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,17 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_course, only: [:show]
+
   def show
   end
 
   private
+
+  def require_enrolled_for_current_course
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      render plain: "Unauthorized", status: :unauthorized
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
